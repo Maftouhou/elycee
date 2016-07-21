512ec31ccfa04600491414835433d47635eed854
thibaultterray <thibault@terray.net>
2016-07-21 17:58:27 +0200

    Ajout du framework Angular JS et mise en place d'un exemple de routing

---
+++ public/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name elycee.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the elycee
+ */
+angular.module('elycee')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
