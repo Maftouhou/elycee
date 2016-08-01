3c91e8ea2c0b97a174c3ab77aa77e582fe05526d
maftouh <maftouh.hassane@gmail.com>
2016-08-02 00:59:08 +0200

    perfecting code

--- resources/views/admin/dashboard/reponse/main_reponse.blade.php
+++ resources/views/admin/dashboard/reponse/main_reponse.blade.php
@@ -2,6 +2,31 @@
 
 @section('content')
 <h2>liste des questions - Choisir une question pour commencer à repondres</h2>
-
-
+<div id="article_container">
+    @if($questions)
+    <table class="article_list">
+        <thead>
+            <tr>
+                <td>Titre</td>
+                <td>Class</td>
+                <td>Status</td>
+            </tr>
+        </thead>
+        <?php $odd = 0; ?>
+        @forelse($questions as $question)
+        <?php $odd++; ?>
+        <tbody>
+            <tr class="{{$odd%2==0?'evenClass':'oddClass'}}">
+                <td><a href="{{url('api/questions', $question->id)}}">{{str_limit($question->title, 7)}}</a></td>
+                <td> {{$question->class}} </td>
+                <td> {{$question->status}} </td>
+            </tr>
+        </tbody>
+        @empty
+            <p>Pas de question à repondre</p>
+        @endforelse
+    </table>
+    @else Vous n'avez pas de QCM à faire
+    @endif
+</div>
 @endsection
