c38030a1ffab2268dfe1889108d01db3bd4a3109
thibaultterray <thibault@terray.net>
2016-08-02 20:01:40 +0200

    style laravel

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,7 +1,8 @@
-<footer>
+<!-- <footer>
     <ul>
         <li>Mention legal&nbsp;</li>
         <li>Copyright &copy;</li>
         <li>Follow us on &origof; &mumap; </li>
     </ul>
 </footer>
+ -->
--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,16 +1,15 @@
 <?php $loginState = Auth::check() ? true : false ?>
 <nav>
     <ul id="navigation">
-        <li><a href="{{url('/')}}">&utrif; Home</a></li>
-        <li><a>Contact &squarf;</a></li>
+        <li><a href="{{url('/')}}" style="color:white;">Home</a></li>
     </ul>
     <ul id="connexion">       
         @if($loginState === true)
-            <li><a href="{{url('api/dashboard')}}">Bonjour {{Auth::user()->username}}</a></li>
-            <li><a id="login" href="{{url('api/logout')}}">&ltrif; Logout </a></li>
+            <li><a href="{{url('api/dashboard')}}" style="color:white;">Bonjour {{Auth::user()->username}}</a></li>
+            <li><a id="login" href="{{url('api/logout')}}">Logout</a></li>
             <li><a id="dashbord" href="{{url('api/dashboard')}}">Dashbord </a></li>
         @else
-            <li><a id="logout" href="{{url('api/login')}}">Login &rtrif;</a></li>
+            <li><a id="logout" href="{{url('api/login')}}">Login</a></li>
         @endif
     </ul>
 </nav>
